453359e20b73406f65090f575a93ef8845d7851e
Jun Tan <ohtianla@gmail.com>
2014-05-05 22:49:17 +0800

    Update JavlibrarySearch.js
    修复brspread搜索url错误

--- JavlibrarySearch.js
+++ JavlibrarySearch.js
@@ -22,7 +22,7 @@ function addSearch(key){
     var btdigg = "<a href = 'http://btdigg.org/search?info_hash=&q=" + key + " ' target= '_blank'>" +"Btdigg"  + "</a>";
     var kitty ="<a href = 'http://www.torrentkitty.com/search/" + key + " ' target= '_blank'>" +"Torrent Kitty"  + "</a>";
     var btsnoop = "<a href = 'http://bitsnoop.com/search/all/" + key + " ' target= '_blank'>" +"Btsnoop"  + "</a>";
-    var btspread = "<a href = 'http://www.btspread.com/search/all/" + key + " ' target= '_blank'>" +"Btspread"  + "</a>";
+    var btspread = "<a href = 'http://www.btspread.com/search/" + key + " ' target= '_blank'>" +"Btspread"  + "</a>";
     var addDiv = '<div id="video_download" class="item"><table><tr>'
                  + "<td class ='header'>" + "BT搜索:" + "</td>"
                  + "<td>"  + btdigg + "</td>"
