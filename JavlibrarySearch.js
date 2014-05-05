605bb1af26d3fe09fd88488fc976e77a8d4222ed
Jun Tan <ohtianla@gmail.com>
2014-05-05 22:40:29 +0800

    Create JavlibrarySearch.js
    懂的人懂

---
+++ JavlibrarySearch.js
@@ -0,0 +1,38 @@
+// ==UserScript==
+// @name        JavlibrarySearch
+// @namespace   JavlibrarySearch
+// @description Add search function for Javlibrary
+// @include     http://www.javlibrary.com/cn/?v=*
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+function getMovieId(){
+    x= document.getElementById("video_id");
+    //alert(x.innerHTML);
+    td = x.getElementsByTagName("td");
+    //alert(td.length);
+    key = td[1].innerHTML;
+    //alert(key);
+    return key;
+}
+
+function addSearch(key){
+   //alert(key);
+    var btdigg = "<a href = 'http://btdigg.org/search?info_hash=&q=" + key + " ' target= '_blank'>" +"Btdigg"  + "</a>";
+    var kitty ="<a href = 'http://www.torrentkitty.com/search/" + key + " ' target= '_blank'>" +"Torrent Kitty"  + "</a>";
+    var btsnoop = "<a href = 'http://bitsnoop.com/search/all/" + key + " ' target= '_blank'>" +"Btsnoop"  + "</a>";
+    var btspread = "<a href = 'http://www.btspread.com/search/all/" + key + " ' target= '_blank'>" +"Btspread"  + "</a>";
+    var addDiv = '<div id="video_download" class="item"><table><tr>'
+                 + "<td class ='header'>" + ":" + "</td>"
+                 + "<td>"  + btdigg + "</td>"
+                 + "<td>" + kitty + "</td>" 
+                 + "<td>" + btsnoop + "</td>"
+                 + "<td>" + btsnoop + "</td>"
+                 + "</tr></table></div>";
+    //alert(addDiv);
+    video_info = document.getElementById("video_info");
+    video_info.insertAdjacentHTML('afterend', addDiv);
+}
+
+addSearch(getMovieId());
